0ff7185763e82963927093e41e3442780b694931
Axel Marazzi <marazzi.axel@gmail.com>
2017-08-20 23:50:29 -0300

    Revert "Ultimo post"

--- js/ultimo.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $.getJSON('https://freelance.axelvf.com.ar/observando/mailchimp.php')
-        .success(function(data) {
-            window.location.href = data[0].link;
-        })
-        .error(function(e) {
-            console.error(e);
-        })
-});
